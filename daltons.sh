bf7732d6461f5c3f0054d43c663eb8b1f24bfa83
Peter Glas <pglas@anwb.nl>
2017-05-30 13:37:42 +0200

    Changed some links to https. Added new keys

--- daltons.sh
+++ daltons.sh
@@ -11,3 +11,4 @@ PROXY=$CLIENT_PROXY
 JAVA_OEPS="-Djavax.net.ssl.trustStore=$CURRENT_DIR/cacerts.jks -Djavax.net.ssl.trustStorePassword=changeit -Djavax.net.debug=not $PROXY"
 JAVA="$JAVA_HOME/bin/java"
 $JAVA $JAVA_OEPS -jar daltons-version-checker.jar
+echo Watchout! uses own keystore!
